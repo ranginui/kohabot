16bc5a6185fa8db138b014cb85a1bc3ba59e4e9c
Chris Cormack <chris@bigballofwax.co.nz>
2009-12-01 20:45:37 +1300

    Updating Licensing information

--- Koha-Bot-Jabber/lib/Koha/Bot/Jabber.pm
+++ Koha-Bot-Jabber/lib/Koha/Bot/Jabber.pm
@@ -64,15 +64,6 @@ connect()
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
 =head1 AUTHOR
 
 Chris Cormack, E<lt>chris@bigballofwax.co.nzE<gt>
@@ -80,10 +71,6 @@ Chris Cormack, E<lt>chris@bigballofwax.co.nzE<gt>
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2007 by Chris Cormack
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.8 or,
-at your option, any later version of Perl 5 you may have available.
 
 
 =cut
